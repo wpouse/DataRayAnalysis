7db0e63f7637e91ade6311478acc1f5f9c7477f8
wpouse <wpouse@berkeley.edu>
2015-10-20 12:39:37 -0700

    Finalized base code
    Gives reasonable values and graphs and outputs as desired

--- pump_beam_analysis.m
+++ pump_beam_analysis.m
@@ -5,21 +5,22 @@ fluence_x = pump_beam.fluence_matrix(pump_beam.max_position(1),:); %fluence valu
 fluence_y = pump_beam.fluence_matrix(:,pump_beam.max_position(2)); %fluence values for column containing max fluence
 
 figure
-plot(1:length(fluence_x), fluence_x.*1/(pump_beam.pixel_size^2)); %currently x axis in units of pixels
+plot(((1:length(fluence_x)).*pump_beam.pixel_size)-pump_beam.pixel_size/2, fluence_x.*1/(pump_beam.pixel_size^2)); 
 title('x-axis Fluence');
-ylabel('Fluence');
-xlabel('Position (in pixel #)');
+ylabel('Fluence (mJ/um^2)');
+xlabel('Position (um)');
 
 
 figure
-plot(1:length(fluence_y), fluence_y.*1/(pump_beam.pixel_size^2)); %x axis in units of pixels
+plot(((1:length(fluence_y)).*pump_beam.pixel_size)-pump_beam.pixel_size/2, fluence_y.*1/(pump_beam.pixel_size^2)); 
 title('y-axis Fluence');
-ylabel('Fluence');
-xlabel('Position (in pixel #)');
+ylabel('Fluence (mJ/um^2)');
+xlabel('Position (um)');
 
 %Define circle for logical indexing
 [rr, cc] = meshgrid(1:length(fluence_x));
-radius = (pump_beam.FWHM_x+pump_beam.FWHM_y)/4; %using radius that is average of FWHM in x and y direction (divide by 4 since FWHM is a diameter)
+%FWHM is in um so converting to pixel
+radius = (pump_beam.FWHM_x+pump_beam.FWHM_y)/(4*pump_beam.pixel_size); %using radius that is average of FWHM in x and y direction (divide by 4 since FWHM is a diameter)
 Circle = sqrt((rr-pump_beam.max_position(2)).^2 +(cc-pump_beam.max_position(1)).^2)<= radius;
 avg_fluence = mean(pump_beam.fluence_matrix(Circle))*1/(pump_beam.pixel_size^2); %divide by pixel size to get mJ/um^2 
 
