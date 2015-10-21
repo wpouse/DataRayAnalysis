449cd6bd3ba9a4c5b61d52ebdf18fa7890278ed7
wpouse <wpouse@berkeley.edu>
2015-10-21 16:54:58 -0700

    Basic screenshot to intensity picture made!
    RGB converter runs much faster, get a reasonable intensity picture.
    Still issue of rectangular screenshot when calculating circular cross
    section. Also need to remove lines from screenshot (interpolate in
    matlab or photoshop)

--- ColorMapping/rgb_converter.m
+++ ColorMapping/rgb_converter.m
@@ -1,25 +1,43 @@
-function intensity = rgb_converter(rgb)
+function intensity = rgb_converter(rgb, rgb_scale, gray_scale)
 %input RGB color (R G B) and outputs the corresponding intensity value
 %defined by DataRay monochromatic colorscale. The grayscale RGB values are
 %just linear (0 0 0) (1 1 1) (2 2 2)...(255 255 255) so can just use the
 %numer as the intensity representation. Problem is normally output DataRay
 %as 16bit tiff but here get 8bit max so can only represent 255 different
 %intensity values
+%{
 rgb_scale = imread('RGBscale.bmp');
 gray_scale = imread('intensity_scale.bmp');
+%}
 
+%%
+%Section to try to make rgb converter more efficient using entire matrix at
+%once 
+%{
+dimensions = size(rgb);
+intensity_matrix = zeros(dimensions(1), dimensions(2));
+%}
+
+
+%%
 %Finding R, G, B rows with rgb we want then calculating intersection to get
 %row
-r_indices = find(rgb_scale(:,1,1) == rgb(1));
-g_indices = find(rgb_scale(:,1,2) == rgb(2));
-b_indices = find(rgb_scale(:,1,3) == rgb(3));
+r_scale = abs(rgb_scale(:,1,1) - rgb(1));
+g_scale = abs(rgb_scale(:,1,2) - rgb(2));
+b_scale = abs(rgb_scale(:,1,3) - rgb(3));
+
+r_indices = find(r_scale == min(r_scale));
+g_indices = find(g_scale == min(g_scale));
+b_indices = find(b_scale == min(b_scale));
 
 row_index = intersect(b_indices, intersect(r_indices, g_indices));
+
+
 i = 1;
 while isempty(row_index)
-    r_indices = intersect(find((rgb_scale(:,1,1) - rgb(1)) <= i), find((rgb_scale(:,1,1) - rgb(1)) >= i));
-    g_indices = intersect(find((rgb_scale(:,1,2) - rgb(2)) <= i), find((rgb_scale(:,1,2) - rgb(2)) >= i));
-    b_indices = intersect(find((rgb_scale(:,1,3) - rgb(3)) <= i), find((rgb_scale(:,1,3) - rgb(3)) >= i));
+    r_indices = find(r_scale <= i);
+    g_indices = find(g_scale <=i);
+    b_indices = find(b_scale <=i);
 
     row_index = intersect(b_indices, intersect(r_indices, g_indices));
     i = i+1;
--- DataRayScreenshotAnalysis.m
+++ DataRayScreenshotAnalysis.m
@@ -5,12 +5,17 @@ x_length = length(image_matrix(:,1,1));
 y_length = length(image_matrix(1,:,1));
 intensity_matrix = zeros(x_length, y_length);
 
+rgb_scale = imread('RGBscale.bmp'); %use as inputs to rgb converter so matlab doesn't have to reread each image for each function call
+gray_scale = imread('intensity_scale.bmp');
+
 %loop through each x,y coordinate extract [r g b] value. Pass rgb into
 %converter which outputs intensity and set that as intensity matrix element
 for i = 1:x_length
     for j = 1:y_length
-        intensity_matrix(i,j) = rgb_converter(reshape(image_matrix(i,j,:), [1, 3]));
+        intensity_matrix(i,j) = rgb_converter(reshape(image_matrix(i,j,:), [1, 3]), rgb_scale, gray_scale);
     end
 end
+
+intensity_matrix = uint8(intensity_matrix); %intensity matrix was originally a double and when writing to image tiff assumes uint8 otherwise it converts by normalizing to 255
 imwrite(intensity_matrix, 'temp_intensity_matrix_from_screnshot.tiff');
 pump_beam_analysis('temp_intensity_matrix_from_screnshot.tiff', pump_power)
