3a6578bf10d22c4eaa773c7db5f4267658e6715a
wpouse <wpouse@berkeley.edu>
2016-02-22 15:57:38 -0800

    Fixed max fluence
    Max fluence returns correct value even when normalized (intensity ratio
    is unchanged from my implementation). Also returns the pump beam profile
    now

--- pump_beam_analysis.m
+++ pump_beam_analysis.m
@@ -1,10 +1,13 @@
-function pump_beam_analysis(pump_beam_image, power, pixel_size)
+function pump_beam = pump_beam_analysis(pump_beam_image, power, pixel_size)
 if nargin<3
     pixel_size = 9.3; %setting default pixel size to 9.3 um
 end
 
 normalization = 0; %0 for false, 1 for true
 %use this as power input to normalize total energy to 1mJ
+%NOTE: power intensity ratio will be messed up since not modified in
+%normalization-so any functions involving that or max_intensity will be
+%wrong!!!
 if strcmp(power, 'norm')
     power = 1;
     normalization = 1;
@@ -19,11 +22,12 @@ if normalization == 1
 end
 
 
-max_fluence = pump_beam.max_intensity * pump_beam.power_intensity_ratio *1/(pump_beam.rep_rate*pump_beam.pixel_size^2); %divide by pixel size to get mJ/um^2, currently fluence matrix using mJ/pixelsize^2
+max_fluence = max(max(pump_beam.fluence_matrix))* 1/(pump_beam.pixel_size^2); %divide by pixel size to get mJ/um^2, currently fluence matrix using mJ/pixelsize^2
 fluence_x = pump_beam.fluence_matrix(pump_beam.max_position(1),:); %fluence values for row containing max fluence
 fluence_y = pump_beam.fluence_matrix(:,pump_beam.max_position(2)); %fluence values for column containing max fluence
 
 figure
+%subtracting pixel_size/2 to plot points at center of pixel
 plot(((1:length(fluence_x)).*pump_beam.pixel_size)-pump_beam.pixel_size/2, fluence_x.*1/(pump_beam.pixel_size^2)); 
 title('x-axis Fluence');
 ylabel('Fluence (mJ/um^2)');
